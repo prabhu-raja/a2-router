a3e8da5dbe89201d1029a9988d12d67a718398eb
prabhu-raja <prabhuraja@gmail.com>
2018-06-14 15:39:34 -0400

    used routerLinkActive routerLinkActiveOptions

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  styles: [`
+    .active {
+      color: #FFF !important;
+    }
+  `],
   templateUrl: '/app/app.component.html'
 })
 export class AppComponent {
