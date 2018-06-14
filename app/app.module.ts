5480b500937961bd5baa5fd177d88901fa3bb26c
prabhu-raja <prabhuraja@gmail.com>
2018-06-14 14:59:44 -0400

    Started Routing🛣

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+// import { FormsModule } from "@angular/forms";
+
+import { appRouting } from "./app.routing";
+
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    // FormsModule,
+    appRouting
+  ],
+  declarations: [
+    AppComponent, 
+    HomeComponent, 
+    AboutComponent, 
+    ContactComponent
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ app/home/home.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "home-page",
+  template: `I am the 🏡 Page.`
+})
+export class HomeComponent {}
